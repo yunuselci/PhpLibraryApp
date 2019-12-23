eb3850e31e6e5dfceae1af319745ca4eb8952900
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2019-12-23 14:13:16 +0300

    v1.7
    +env.example eklendi
    +gitignore düzenlendi

--- db_connect.php
+++ db_connect.php
@@ -14,8 +14,6 @@ $db_host = getenv('db_host');
 $db_name = getenv('db_name');
 $db_user = getenv('db_user');
 $db_pass = getenv('db_pass');
-
-
 try {
     $db_connect = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_user, $db_pass);
     $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
